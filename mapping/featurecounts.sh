b2a1d0f1fb1594856c35abb99521e8a63005e9b4
yiruiS <94996814+yiruiS@users.noreply.github.com>
2025-07-14 11:49:38 -0400

    Update featurecounts.sh

--- mapping/featurecounts.sh
+++ mapping/featurecounts.sh
@@ -1,10 +1,10 @@
 module load Subread/2.0.6-GCC-11.3.0
 
-featureCounts -p --countReadPairs -t exon -g Parent -a /scratch/ys33815/sun/Zm-W22-REFERENCE-NRGENE-2.0_Zm00004b.1.gff3 \
--o ../endo6_38_counts.txt SRR1169626Aligned.sortedByCoord.out.bam SRR1169633Aligned.sortedByCoord.out.bam SRR1169641Aligned.sortedByCoord.out.bam \
-SRR1169627Aligned.sortedByCoord.out.bam SRR1169634Aligned.sortedByCoord.out.bam SRR1169642Aligned.sortedByCoord.out.bam \
-SRR1169628Aligned.sortedByCoord.out.bam SRR1169635Aligned.sortedByCoord.out.bam SRR1169643Aligned.sortedByCoord.out.bam \
-SRR1169629Aligned.sortedByCoord.out.bam SRR1169636Aligned.sortedByCoord.out.bam SRR1169644Aligned.sortedByCoord.out.bam \
-SRR1169630Aligned.sortedByCoord.out.bam SRR1169637Aligned.sortedByCoord.out.bam SRR1169645Aligned.sortedByCoord.out.bam \
-SRR1169631Aligned.sortedByCoord.out.bam SRR1169639Aligned.sortedByCoord.out.bam SRR1169646Aligned.sortedByCoord.out.bam \
-SRR1169632Aligned.sortedByCoord.out.bam SRR1169640Aligned.sortedByCoord.out.bam SRR1169647Aligned.sortedByCoord.out.bam 
+featureCounts -p --countReadPairs  -t exon -g Parent -a /scratch/ys33815/sun/Zm-W22-REFERENCE-NRGENE-2.0_Zm00004b.1.gff3 \
+-o ../nodup_endo6_38_counts.txt SRR1169626Aligned_nodup_q20.bam SRR1169633Aligned_nodup_q20.bam SRR1169641Aligned_nodup_q20.bam \
+SRR1169627Aligned_nodup_q20.bam SRR1169634Aligned_nodup_q20.bam SRR1169642Aligned_nodup_q20.bam \
+SRR1169628Aligned_nodup_q20.bam SRR1169635Aligned_nodup_q20.bam SRR1169643Aligned_nodup_q20.bam \
+SRR1169629Aligned_nodup_q20.bam SRR1169636Aligned_nodup_q20.bam SRR1169644Aligned_nodup_q20.bam \
+SRR1169630Aligned_nodup_q20.bam SRR1169637Aligned_nodup_q20.bam SRR1169645Aligned_nodup_q20.bam \
+SRR1169631Aligned_nodup_q20.bam SRR1169639Aligned_nodup_q20.bam SRR1169646Aligned_nodup_q20.bam \
+SRR1169632Aligned_nodup_q20.bam SRR1169640Aligned_nodup_q20.bam SRR1169647Aligned_nodup_q20.bam
