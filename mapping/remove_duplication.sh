fc2c379c72959b1b688e03bd7c299fe8562caf06
yiruiS <94996814+yiruiS@users.noreply.github.com>
2025-07-14 13:51:03 -0400

    Update remove_duplication.sh

--- mapping/remove_duplication.sh
+++ mapping/remove_duplication.sh
@@ -2,6 +2,5 @@ module load picard/2.27.5-Java-15
 
 for f in *Aligned.sortedByCoord.out.bam
 do
-java -jar $EBROOTPICARD/picard.jar MarkDuplicates -I ${f%%.*}.sortedByCoord.out.bam -O ${f%%.*}_nodup_q20.bam -M marked_dup_metrics.txt --REMOVE_DUPLICATES tr
-ue
+java -jar $EBROOTPICARD/picard.jar MarkDuplicates -I ${f%%.*}.sortedByCoord.out.bam -O ${f%%.*}_nodup_q20.bam -M marked_dup_metrics.txt --REMOVE_DUPLICATES true
 done
