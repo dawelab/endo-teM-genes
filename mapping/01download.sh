b5c2aa8fdf84c0d72191ae3ee29f5c255d1a3405
yiruiS <94996814+yiruiS@users.noreply.github.com>
2025-07-14 13:50:01 -0400

    Create 01download.sh

---
+++ mapping/01download.sh
@@ -0,0 +1,46 @@
+ml SRA-Toolkit/3.0.3-gompi-2022a  
+ml parallel-fastq-dump/0.6.7-gompi-2022a
+
+prefetch SRR1169626
+prefetch SRR1169627
+prefetch SRR1169628
+prefetch SRR1169629
+prefetch SRR1169630
+prefetch SRR1169631
+prefetch SRR1169632
+prefetch SRR1169633
+prefetch SRR1169634
+prefetch SRR1169635
+prefetch SRR1169636
+prefetch SRR1169637
+prefetch SRR1169639
+prefetch SRR1169640
+prefetch SRR1169641
+prefetch SRR1169642
+prefetch SRR1169643
+prefetch SRR1169644
+prefetch SRR1169645
+prefetch SRR1169646
+prefetch SRR1169647
+
+fastq-dump SRR1169626 --split-3
+fastq-dump SRR1169627 --split-3
+fastq-dump SRR1169628 --split-3
+fastq-dump SRR1169629 --split-3
+fastq-dump SRR1169630 --split-3
+fastq-dump SRR1169631 --split-3
+fastq-dump SRR1169632 --split-3
+fastq-dump SRR1169633 --split-3
+fastq-dump SRR1169634 --split-3
+fastq-dump SRR1169635 --split-3
+fastq-dump SRR1169636 --split-3
+fastq-dump SRR1169637 --split-3
+fastq-dump SRR1169639 --split-3
+fastq-dump SRR1169640 --split-3
+fastq-dump SRR1169641 --split-3
+fastq-dump SRR1169642 --split-3
+fastq-dump SRR1169643 --split-3
+fastq-dump SRR1169644 --split-3
+fastq-dump SRR1169645 --split-3
+fastq-dump SRR1169646 --split-3
+fastq-dump SRR1169647 --split-3
