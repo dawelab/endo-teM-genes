4c713533dbc43465e4ae212302e2f6199c1325a6
yiruiS <94996814+yiruiS@users.noreply.github.com>
2025-07-22 16:00:53 -0400

    Update 02.3endosperm_specific_genes.R

--- identification/02.3endosperm_specific_genes.R
+++ identification/02.3endosperm_specific_genes.R
@@ -1,7 +1,15 @@
+specific <- read.delim("~/Documents/lab/b73.all.6_38.txt", comment.char="#")
 
-
-specific<-b73.all.6_38
+#calculate the max TPM in all the endosperm tissues
 specific$maxendo<-apply(specific[,25:41],1,max)
+
+#calculte the max TPM in all the sporophyte tissues.
 specific$max<-apply(specific[,c(14,16:23)],1,max)
+
+#calcilate the expression FC between endosperm and sporophyte tissue.
 specific$FC<-(specific$maxendo+1)/(specific$max+1)
+
+#select the endosperm specific genes using FC >=5 and endosperm TPM >=20
 specific<-specific[which(specific$maxendo>=20&specific$FC>=5),]
+
+
