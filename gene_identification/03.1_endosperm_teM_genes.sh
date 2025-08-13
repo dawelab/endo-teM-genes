b35f60c84a50c443ea396f2250995efe5fccc026
yiruiS <94996814+yiruiS@users.noreply.github.com>
2025-08-12 21:57:27 -0400

    Update 03.1_endosperm_teM_genes.sh

--- gene_identification/03.1_endosperm_teM_genes.sh
+++ gene_identification/03.1_endosperm_teM_genes.sh
@@ -50,6 +50,5 @@ colnames(all_body_genes)<-'b73'
 b73_core_genes <- read.csv("~/Documents/lab/b73_core_genes.txt", sep="")
 core_gene_te_like_gene<-merge(b73_core_genes,all_body_genes,by.y = "b73",by.x = "x", all=F)
 core_gene_te_like_gene<-core_gene_te_like_gene$x
-#write.table(core_gene_te_like_gene,"~/Documents/lab/megpeg/all_BM_5FC_core.bed",quote = F,row.names = F,
+#write.table(core_gene_te_like_gene,"~/Documents/lab/megpeg/endosperm_teM_genes.bed",quote = F,row.names = F,
 #            col.names = F,sep = "\t")
-
